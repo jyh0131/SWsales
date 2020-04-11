603bf211d6af42665dbba3eac210381215d0bc9e
jamesareumin <hothihi5@gmail.com>
2020-04-11 22:44:04 +0900

    발주 이력 리스트 삭제 구현

--- src/swsales/handler/product/SupplierOrder1DeleteHandler.java
+++ src/swsales/handler/product/SupplierOrder1DeleteHandler.java
@@ -1,15 +1,36 @@
 package swsales.handler.product;
 
+import java.sql.Connection;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import swsales.dao.SupplierOrderDao;
+import swsales.jdbc.JDBCUtil;
+import swsales.model.SupplierOrder;
 import swsales.mvc.CommandHandler;
 
 public class SupplierOrder1DeleteHandler implements CommandHandler{
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		// TODO Auto-generated method stub
+		
+		int soNo = Integer.parseInt(req.getParameter("no"));
+		Connection conn = null;
+		
+		try {
+			conn = JDBCUtil.getConnection();
+			SupplierOrderDao dao = SupplierOrderDao.getInstance();
+			SupplierOrder so = new SupplierOrder(soNo);
+			dao.deleteSupplierOrder(conn, so);
+			res.sendRedirect(req.getContextPath()+"/product/supplierOrderList1.do");
+			return null;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(conn);
+		}
 		return null;
 	}
 
